ba27fcd1746098255226da099ebefcb46788f77f
Rustam4457 <punk60826@gmail.com>
2018-11-19 16:55:58 +0200

    adding layout and converting html files to pug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
@@ -17,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3001);
