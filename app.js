f5362d0f35bb457080c65f16a7930712db5f355a
Rustam4457 <punk60826@gmail.com>
2018-11-26 22:17:39 +0200

    'fixing'

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+
 
 app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3001);
